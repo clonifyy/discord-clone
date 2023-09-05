c172c7356844c0f59dd41382dcedc054ba43e538
vien.huynh <vien.huynh@VienHuynh.local>
2023-09-05 11:47:10 +0700

    feat: set up prisma & create models

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,22 @@
+import { db } from "@/lib/db";
+import { initialProfile } from "@/lib/initial-profile";
+import { redirect } from "next/navigation";
+
+const SetupPage = async () => {
+  const profile = await initialProfile();
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+  return <div>create server</div>;
+};
+
+export default SetupPage;
