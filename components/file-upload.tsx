2b1bf2c697217b370ad262a45831b31fc859080b
vien.huynh <vien.huynh@VienHuynh.local>
2023-09-06 13:51:34 +0700

    feat: navigation sidebar

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -17,6 +17,12 @@ export default function FileUpload({ onChange, value, endPoint }: Props) {
     return (
       <div className="relative h-20 w-20">
         <Image fill src={value} alt="upload" className="rounded-full" />
+        <button
+          onClick={() => onChange("")}
+          className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+        >
+          <X className="h-4 w-4" />
+        </button>
       </div>
     );
   }
---
+++ components/navigation/navigation-item.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { ActionTooltip } from "../action-tooltip";
+import { useParams, useRouter } from "next/navigation";
+import Image from "next/image";
+
+interface Props {
+  id: string;
+  imageUrl: string;
+  name: string;
+}
+export const NavigationItem = ({ id, imageUrl, name }: Props) => {
+  const params = useParams();
+  const router = useRouter();
+
+  const onClick = () => {
+    router.push(`/servers/${id}`);
+  };
+  return (
+    <ActionTooltip side="right" align="center" label={name}>
+      <button onClick={onClick} className="group relative flex items-center">
+        <div
+          className={cn(
+            "absolute, left-0 bg-primary rounded-r-full transition-all w-[4px]",
+            params?.serverId !== id && "group-hover:h-[20px]",
+            params?.serverId === id ? "h-[36px]" : "h-[8px]"
+          )}
+        />
+        <div
+          className={cn(
+            "relative group flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden",
+            params?.serverId === id &&
+              "bg-primary/10 text-primary rounded-[16px]"
+          )}
+        >
+          <Image fill src={imageUrl} alt={`${name} channel`} />
+        </div>
+      </button>
+    </ActionTooltip>
+  );
+};
