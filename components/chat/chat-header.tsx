9af3d27aae6099eec6b13ec9333f0bef2e1d7710
Vien <vien200203@gmail.com>
2023-09-27 12:01:50 +0700

    fix bug

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -16,7 +16,9 @@ export const ChatHeader = ({ name, serverId, type, imgUrl }: Props) => {
       {type === "channel" && (
         <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
       )}
-      <p className="font-semibold text-md text-black dark:text-white">{name}</p>
+      <p className="font-semibold text-md text-black dark:text-white">
+        {name} {""}
+      </p>
     </div>
   );
 };
